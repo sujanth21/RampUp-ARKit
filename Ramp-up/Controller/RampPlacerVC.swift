657a400ff5d039a428d15d6136012f84ce857475
Sujanth <sujanth1987@gmail.com>
2017-08-22 13:38:00 +1000

    Refactored VC name and structure

---
+++ Ramp-up/Controller/RampPlacerVC.swift
@@ -0,0 +1,58 @@
+//
+//  RampPlacerVC.swift
+//  Ramp-up
+//
+//  Created by Sujanth on 22/8/17.
+//  Copyright © 2017 Sujanth. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import SceneKit
+
+class RampPlacerVC: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        sceneView.delegate = self
+        
+        sceneView.showsStatistics  = true
+        
+        let scene = SCNScene(named: "")!
+        
+        sceneView.scene = scene
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingSessionConfiguration()
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    
+    
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        
+    }
+
+}
