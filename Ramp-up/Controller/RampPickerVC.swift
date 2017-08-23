7c306f7223905a37c7ce73963a65d167f0c94cc5
Sujanth <sujanth1987@gmail.com>
2017-08-23 11:24:09 +1000

    Second model added

--- Ramp-up/Controller/RampPickerVC.swift
+++ Ramp-up/Controller/RampPickerVC.swift
@@ -31,22 +31,30 @@ class RampPickerVC: UIViewController {
         
         view.insertSubview(sceneView, at: 0)
         
-        let scene = SCNScene(named: "art/scnassets/ramps.scn")!
+        preferredContentSize = size
+        
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
         sceneView.scene = scene
+        
         
         let camera = SCNCamera()
         camera.usesOrthographicProjection = true
         scene.rootNode.camera = camera
         
-        let obj = SCNScene(named: "art.scnassets/pipe.dae")
-        let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
-        
+        var obj = SCNScene(named: "art.scnassets/pipe.dae")
+        var node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
         node?.scale = SCNVector3Make(0.0022, 0.0022, 0.0022)
         node?.position = SCNVector3Make(-1, 0.7, -1)
-        
         scene.rootNode.addChildNode(node!)
         
-        preferredContentSize = size
+        obj = SCNScene(named: "art.scnassets/pyramid.dae")
+        node = obj?.rootNode.childNode(withName: "pyramid", recursively: true)!
+        node?.scale = SCNVector3Make(0.0058, 0.0058, 0.0058)
+        node?.position = SCNVector3Make(-1, -0.65, -1)
+        scene.rootNode.addChildNode(node!)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
